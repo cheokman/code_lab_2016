eaca3a38681f98ad52ab2d475960769aff71786d
Ben Wu <wucheokman@gmail.com>
2016-08-27 09:51:44 +0800

    change valid? to valid_key? and add [] and []= method

--- serializer/box_pack.rb
+++ serializer/box_pack.rb
@@ -23,14 +23,23 @@ class Box < Hash
     volatile: 1 << 1
   }
 
+  def []=(key, value)
+    raise Error.new(name) if valid_key?(key)
+    self[name.to_s] = value
+  end
+
+  def [](key)
+    self[name.to_s]
+  end
+
   def method_missing(method, *args)
     m = method.to_s
     key = m.gsub(/=$/,'')
-    raise Error "Invalid key name" if valid?(key)
+    raise Error "Invalid key name" if valid_key?(key)
     m.match(/=$/) ? self.send("[#{key}]=", *args) : self[key]
   end
 
-  def valid?(key)
+  def valid_key?(key)
     !key.match(/^_/).nil?
   end
 end
