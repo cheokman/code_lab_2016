153f153c1e5dfa7d5147c72f326d4ffc8d552732
Ben Wu <wucheokman@gmail.com>
2016-09-02 11:56:28 +0800

    add Array extract_options! methods and replace trigger_count with fetch methods

--- rule_matcher/rules_methods.rb
+++ rule_matcher/rules_methods.rb
@@ -1,3 +1,9 @@
+class Array
+  def extract_options!
+    last.is_a?(::Hash) ? pop : {}
+  end unless defined? Array.new.extract_options!
+end
+
 module Gear
   module RulesMethods
     # Define rule for match symbol
@@ -7,12 +13,12 @@ module Gear
       obj = obj.to_sym
 
       rule = Rule.new
-      rule.symbol_id   = options[:symbol_id]
-      rule.symbol_name = options[:symbol_name]
-      rule.multiplier  = options[:multiplier]
-      rule.trigger  = options[:trigger] 
-      rule.trigger_count  = options[:trigger_count] if rule.trigger.present?
-      rule.block       = block
+      rule.symbol_id      = options[:symbol_id]
+      rule.symbol_name    = options[:symbol_name]
+      rule.multiplier     = options[:multiplier]
+      rule.trigger        = options[:trigger]
+      rule.trigger_count  = options.fetch(:trigger_count,1).to_i if rule.trigger.present?
+      rule.block          = block
 
       defined_rules[obj] ||= []
       defined_rules[obj] << rule
