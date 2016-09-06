aa665521198857a1c20894d3b61d6b481c21b912
Ben Wu <wucheokman@gmail.com>
2016-09-06 18:24:27 +0800

    add context to saperate the test case

--- entity_relational_mapping/spec/erm_spec.rb
+++ entity_relational_mapping/spec/erm_spec.rb
@@ -1,36 +1,51 @@
 require 'spec_helper'
 
- describe ERM do
-   context "when using models registry from config" do
-     it "has model registry" do
+describe ERM do
+  describe ".model" do
+    it "has model registry" do
       expect(described_class.models).to be_a(Array)
       expect(described_class.models).to be_empty
-     end
+    end
+  end
 
-     it "can registry same class once" do
-       class A; end
-       class B; end
-       described_class.register_model(A)
-       expect(described_class.models).to include(A)
-       described_class.register_model(B)
-       expect(described_class.models).to include(B)
-       described_class.register_model(A)
-       expect(described_class.models).to include(A)
-     end
+  describe ".register_model" do
+    it "can registry same class once" do
+      class A; end
+      class B; end
+      described_class.register_model(A)
+      expect(described_class.models).to include(A)
+      described_class.register_model(B)
+      expect(described_class.models).to include(B)
+      described_class.register_model(A)
+      expect(described_class.models).to include(A)
+    end
+  end
 
-     it "has configuration instance" do
+  describe ".configuration" do
+    it "has configuration instance" do
       expect(described_class.configuration).to be_instance_of(ERM::Configuration)
-     end
+    end
+  end
 
-     it "has accept config block" do
-       described_class.config {|config| config.finalize = false}
+  describe ".config" do
+    context "when no block supplied" do
+      it "returns the configuration singleton" do
+        expect(described_class.config).to be(described_class.configuration)
+      end
+    end
 
-       expect(described_class.configuration.finalize).to be_falsy
+    context "when a block is supplied" do
+      before do
+        described_class.config {|config| config.finalize = false}
+      end
 
-       described_class.config {|config| config.finalize = true}
+      after do
+        described_class.config {|config| config.finalize = true}
+      end
 
-       expect(described_class.configuration.finalize).to be_truthy
-
-     end
-   end
- end 
+      it "has accept config block" do
+        expect(described_class.config.finalize).to be_falsy
+      end
+    end
+  end
+end
