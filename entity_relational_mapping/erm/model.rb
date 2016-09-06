7dcc550d29c21b7f113c11efa9a19193ed90ea1b
Ben Wu <wucheokman@gmail.com>
2016-09-06 09:45:24 +0800

    add valid name methods

--- entity_relational_mapping/erm/model.rb
+++ entity_relational_mapping/erm/model.rb
@@ -2,7 +2,7 @@ module ERM
   module Model
     
     def self.included(base)
-      base.extend         ClassMethods
+      base.extend ClassMethods
       base.class_eval { include InstanceMethods }
       base.class_eval { include InstanceMethods::Constructor }
       ERM.register_model(base)
@@ -22,6 +22,13 @@ module ERM
         attribute_set << Attribute.build(type, options.merge(:name => name))
         self
       end
+
+      def assert_valid_name(name)
+        if instance_methods.include?(:attribute_set) && name.to_sym == :attribute_set
+          raise ArgumentError, "#{name.inspect} is not allowed as an attribute name"
+        end
+      end
+
     end
   end
 end
