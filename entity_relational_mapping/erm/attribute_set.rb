6440dc421eeb3e3329d8394af731d76f2180d0a1
Ben Wu <wucheokman@gmail.com>
2016-09-06 09:27:46 +0800

    add access methods

--- entity_relational_mapping/erm/attribute_set.rb
+++ entity_relational_mapping/erm/attribute_set.rb
@@ -11,9 +11,36 @@ module ERM
 
     def each
       return to_enum unless block_given?
-      @index.each { |name, attribute| yield attribute if name.kind_of?(Symbol) }
+      @mapping.each { |name, attribute| yield attribute if name.kind_of?(Symbol) }
       self
     end
+
+    def merge(attributes)
+      attributes.each { |attribute| self << attribute }
+      self
+    end
+
+    def <<(attribute)
+      self[attribute.name] = attribute
+      attribute.define_accessor_methods(self) if attribute.finalized?
+      self
+    end
+
+    def [](name)
+      @mapping[name]
+    end
+
+    def []=(name, attribute)
+      @attributes << attribute
+      update_mapping(name, attribute)
+    end
+
+    def reset
+      merge_attributes(@parent) if @parent
+      merge_attributes(@attributes)
+      self
+    end
+
 
     def define_reader_method(attribute, method_name, visibility)
       define_method(method_name) { attribute.get(self) }
@@ -25,6 +52,42 @@ module ERM
       send(visibility, method_name)
     end
 
+    def get(object)
+      each_with_object({}) do |attribute, attributes|
+        name = attribute.name
+        attributes[name] = object.__send__(name) if attribute.public_reader?
+      end
+    end
+
+    def set(object, attributes)
+      coerce(attributes).each do |name, value|
+        writer_name = "#{name}="
+        if object.allowed_writer_methods.include?(writer_name)
+          object.__send__(writer_name, value)
+        end
+      end
+    end
+
+    def set_defaults(object, filter = method(:skip_default?))
+      each do |attribute|
+        next if filter.call(object, attribute)
+        attribute.set_default_value(object)
+      end
+    end
+
+    private
+
+    def skip_default?(object, attribute)
+      attribute.lazy? || attribute.defined?(object)
+    end
+
+    def merge_attributes(attributes)
+      attributes.each { |attribute| update_mapping(attribute.name, attribute) }
+    end
+
+    def update_mapping(name, attribute)
+      @mapping[name] = @mapping[name.to_s.freeze] = attribute
+    end
 
   end
 end
