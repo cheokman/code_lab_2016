38f60bb64ed13ba0725e2d6a8a2ff37f36cc5b60
Ben Wu <wucheokman@gmail.com>
2016-09-06 22:04:47 +0800

    add configuration spec

--- entity_relational_mapping/spec/configuration_spec.rb
+++ entity_relational_mapping/spec/configuration_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe ERM::Configuration do
-  
+  let(:config) {described_class.new}
+  context ".initialize" do
+    it "has default finalize value" do
+      expect(config.finalize).to be_truthy
+    end
+  end
+
+  describe ".to_h" do
+    it "has convert to hash" do
+      expect(config.to_h).to eq({finalize: true})
+    end
+  end
 end
