17eeb391dd06951cffbf3acb68a3f56334db0465
Ben Wu <wucheokman@gmail.com>
2016-09-07 22:29:37 +0800

    add class methods test case

--- entity_relational_mapping/spec/erm/model_spec.rb
+++ entity_relational_mapping/spec/erm/model_spec.rb
@@ -6,9 +6,26 @@ describe ERM::Model do
       include ERM::Model
     end
   end
-  describe "#attribute" do
+  describe "class methods" do
+    it "has assert_valid_name class method" do
+      expect(user.private_methods).to include(:assert_valid_name)
+    end
+
     it "has attribute class method" do
       expect(user).to respond_to(:attribute).with(3).arguments
+    end
+
+    describe "#assert_valid_name" do
+      it "accpet any name except attribute_set name" do
+        expect {user.send(:assert_valid_name, 'attribute_set') }.to raise_error(ArgumentError)
+        expect {user.send(:assert_valid_name, 'any_name') }.not_to raise_error
+      end
+    end
+
+    describe "#attribute" do
+      let(:attribute) do
+        Class.new
+      end
     end
   end
 end
