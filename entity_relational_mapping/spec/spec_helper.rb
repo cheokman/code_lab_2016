68243560ee2caa9cda8c97c9c655c3bf1be969a5
Ben Wu <wucheokman@gmail.com>
2016-09-07 10:54:49 +0800

    reset model before running each test case

--- entity_relational_mapping/spec/spec_helper.rb
+++ entity_relational_mapping/spec/spec_helper.rb
@@ -1,3 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'erm'
 require 'rspec'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    ERM.instance_variable_set(:@models, [])
+  end
+end
