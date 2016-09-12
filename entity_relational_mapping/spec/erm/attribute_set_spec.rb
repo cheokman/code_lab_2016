235f5bbe608b719bffe56152dbcd8fc26e566329
Ben Wu <wucheokman@gmail.com>
2016-09-12 12:43:06 +0800

    add module check for AttributeSet

--- entity_relational_mapping/spec/erm/attribute_set_spec.rb
+++ entity_relational_mapping/spec/erm/attribute_set_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe ERM::AttributeSet do
+  describe "#create" do
+    context "when include as module" do
+      let(:user) do
+        Class.new {
+          extend ERM::AttributeSet.create(self)
+        }
+      end
+        
+      before(:each) do
+      end
 
+      it "is descendant of module" do
+        expect(described_class.ancestors).to include(Module)
+      end
+    end
+  end
 end
