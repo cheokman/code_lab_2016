3b9bdafcb90fd3ef20eea0a147d14e07ea1aa19d
Ben Wu <wucheokman@gmail.com>
2016-10-03 21:29:08 +0800

    add rails and sinatra and config

--- axle/lib/axle.rb
+++ axle/lib/axle.rb
@@ -2,4 +2,20 @@ require 'axle/configuration'
 
 module Axle
   Undefined = Object.new.freeze
+
+  def self.config
+    @config ||= Configuration.new
+  end
+
+  def self.base_path
+    @config.base_path
+  end
+end
+
+if defined?(Rails)
+  require 'axle/rails'
+end
+
+if defined?(Sinatra)
+  require 'axle/sinatra'
 end
