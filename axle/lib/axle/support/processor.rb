b6cb2a3654671f97cb2ac6fa81d0b7cb28cd06a9
Ben Wu <wucheokman@gmail.com>
2016-10-04 23:22:10 +0800

    add processor module

---
+++ axle/lib/axle/support/processor.rb
@@ -0,0 +1,25 @@
+module Axle
+  module Processor
+    def start
+    end
+
+    def finish
+    end
+
+    def process
+      pre_update
+      update
+      post_update
+    end
+
+private    
+    def pre_update
+    end
+
+    def update
+    end
+
+    def post_update
+    end
+  end
+end
