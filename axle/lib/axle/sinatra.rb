30bd6acc5b46382a3d9cda9eea1dc0f6112879dd
Ben Wu <wucheokman@gmail.com>
2016-10-04 23:19:34 +0800

    add get and add return 200 for post return

--- axle/lib/axle/sinatra.rb
+++ axle/lib/axle/sinatra.rb
@@ -6,8 +6,13 @@ module Axle
       headers 'X-Powered-By' => "Axle #{Axle::Version}"
     end
 
+    get "/" do
+      
+    end
+
     post "#{Axle.base_path}/play" do #need to implement API Versioning
       data = param['data']
+      200
     end
   end
 end
