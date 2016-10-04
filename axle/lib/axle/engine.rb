c9eb3f9aa113112697053e28132383313b261793
Ben Wu <wucheokman@gmail.com>
2016-10-04 23:19:04 +0800

    update server base path

--- axle/lib/axle/engine.rb
+++ axle/lib/axle/engine.rb
@@ -6,7 +6,7 @@ module Axle
     config.after_initialize do |app|
       if Axle.mount_at
         app.routes.prepend do
-          mount Axle::Engine => Axle.mount_at, as: "#{Alxe.base_path}"
+          mount Axle::Engine => Axle.mount_at, as: "#{Axle.service_base_path}"
         end
       end
     end
