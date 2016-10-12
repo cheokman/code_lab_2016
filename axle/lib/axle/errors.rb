9c38e558a5858ec32d1423b1969c4ce0e86498e1
Ben Wu <wucheokman@gmail.com>
2016-10-12 18:31:48 +0800

    require message error

--- axle/lib/axle/errors.rb
+++ axle/lib/axle/errors.rb
@@ -1,3 +1,5 @@
 require "axle/errors/axle_errors"
 require 'axle/errors/observer_set_name_error'
 require 'axle/errors/observer_type_error'
+require 'axle/errors/message_missing_required_field_error'
+require 'axle/errors/message_missing_request_info_error'
