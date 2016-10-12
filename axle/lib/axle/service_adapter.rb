fc36b87e96b2bd8c9957894c48f1669ad2a8dcb1
Ben Wu <wucheokman@gmail.com>
2016-10-12 18:32:22 +0800

    add build_context, extract_message_data

--- axle/lib/axle/service_adapter.rb
+++ axle/lib/axle/service_adapter.rb
@@ -1,23 +1,32 @@
 module Axle
   module ServiceAdapter
-    extend Processor
-    extend Serializer
-
+    
     def self.included(descendant)
       descendant.extend ClassMethods
       descendant.class_eval{include InstanceMethods}
+      descendant.instance_variable_set("@message_data", nil)
+      descendant.instance_variable_set("@message",nil)
+      descendant.instance_variable_set("@result",{})
+      #descendant.instance_variable_set("@context", {})
     end
     
     module ClassMethods
+
+      def self.extended(descendant)
+        descendant.extend Processor
+        descendant.extend Serializer
+      end
+      
       private
 
       def before_update
-        default_result
         parse_params
+        build_message
+        build_context
       end
 
       def update
-        
+        Axle.notify_observers(@context)
       end
 
       def after_update
@@ -25,21 +34,34 @@ module Axle
       end
 
       def parse_params
-        
+        @message_data = deserialize_value(extract_message_data)
+      end
+
+      def extract_message_data
+        {}
+      end
+
+      def build_message
+        @message_data.merge!(request_info)
+        @message = MessageFactory.build(@message_data)
+      end
+
+      def build_context
+        @context[:message] = @message
       end
 
       def default_result
-        @result = { 
+        result = { 
                      status: 200,
                        text: 'OK'
                    }
       end
 
-      def respond_with_result(result)
-        { 
+      def respond_with_result
+        default_result.merge({ 
           status: result[:status],
-          text: result.to_json
-        }
+            text: serialize_value(result)
+        })
       end
     end
 
